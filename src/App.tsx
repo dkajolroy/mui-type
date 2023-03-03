a608fa0253288835a02bb8ad5fdf5c701c7a7ded
kajolroy <shrikajol@gmail.com>
2023-03-03 19:19:09 +0600

    create theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { colors, createTheme, ThemeProvider } from "@mui/material";
 import { Route, Routes } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
@@ -10,16 +11,26 @@ import FlexboxPage from "./pages/FlexboxPage";
 import HomePage from "./pages/HomePage";
 import TypographyPage from "./pages/TypographyPage";
 
+const theme = createTheme({
+  palette: {
+    secondary: {
+      main: colors.orange[400],
+    },
+  },
+});
+
 export default function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/typography" element={<TypographyPage />} />
-        <Route path="/button" element={<ButtonPage />} />
-        <Route path="/flex" element={<FlexboxPage />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/typography" element={<TypographyPage />} />
+          <Route path="/button" element={<ButtonPage />} />
+          <Route path="/flex" element={<FlexboxPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,6 +3,7 @@ export default function HomePage() {
   return (
     <Box>
       <h1>Home Page</h1>
+      <Box width={100} height={100} bgcolor="secondary.main"></Box>
     </Box>
   );
 }
